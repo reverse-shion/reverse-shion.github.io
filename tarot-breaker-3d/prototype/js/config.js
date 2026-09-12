fab75f4fe71f19aed6bdb5d5ecb2d3453af715c4
reverse-shion <kisaki.aiki@gmail.com>
2026-09-12 18:26:22 +0900

    chore: set Star Country visual version

--- tarot-breaker-3d/prototype/js/config.js
+++ tarot-breaker-3d/prototype/js/config.js
@@ -1,4 +1,4 @@
-export const VERSION = 'p2-1.9.0';
+export const VERSION = 'star-country-v1.0';
 export const ROOT = new URL('../../../', import.meta.url);
 export function asset(path) {
   const url = new URL(path, ROOT);
