73a1b0b7a302ccead016c5a003f46873b615d04d
reverse-shion <kisaki.aiki@gmail.com>
2026-09-12 18:45:29 +0900

    chore: bump Star Country visual version to v1.1

--- tarot-breaker-3d/prototype/js/config.js
+++ tarot-breaker-3d/prototype/js/config.js
@@ -1,4 +1,4 @@
-export const VERSION = 'star-country-v1.0';
+export const VERSION = 'star-country-v1.1';
 export const ROOT = new URL('../../../', import.meta.url);
 export function asset(path) {
   const url = new URL(path, ROOT);
