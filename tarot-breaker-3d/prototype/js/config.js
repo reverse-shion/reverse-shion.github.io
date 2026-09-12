2a7b8105b9e5c4edee6a7d8fa26d6959ed9b56b7
reverse-shion <kisaki.aiki@gmail.com>
2026-09-12 10:32:30 +0900

    chore: bump phase 2 prototype cache version to 1.5

--- tarot-breaker-3d/prototype/js/config.js
+++ tarot-breaker-3d/prototype/js/config.js
@@ -1,4 +1,4 @@
-export const VERSION = 'p2-1.4.0';
+export const VERSION = 'p2-1.5.0';
 export const ROOT = new URL('../../../', import.meta.url);
 export function asset(path) {
   const url = new URL(path, ROOT);
